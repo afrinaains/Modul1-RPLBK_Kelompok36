7c58dce9187c541ba8c1d71873ab20a10d99e2ee
Jeremy <jeremymartinsimanjuntak@gmail.com>
2021-09-08 13:05:38 +0700

    Tugas Modul 1-RPLBK_Kelompok 36
    File Tugas

---
+++ Tugas_Modul1_Kel36/script.js
@@ -0,0 +1,28 @@
+const final = async () => {
+    await output();
+    await akhir ();
+}
+const nama = () => {
+    namaSaya = document.getElementById("namaSaya").value;
+  (namaSaya == "" || !namaSaya) ?
+      alert ("Data Masih Kosong!!!") || console.log("Silakan isi nama anda terlebih dahulu!") : final()
+}
+const akhir = () => 
+    console.log ("Halo kak!");
+
+const output = () => {
+    hasilnama= namaSaya;
+    setTimeout( () => console.log (hasilnama) ,2000) ;
+    setTimeout( () => console.log ("Selamat Datang di Tugas Modul 1 RPLBK Kelompok 36") ,1000) ;
+    setTimeout( () => console.log ("Semoga betah ya kak! fufu"), 3000);
+}
+const umur = () => {
+    umurSaya = document.getElementById("umurSaya").value;
+  (umurSaya == "" || !umurSaya) ?
+      alert ("Data Masih Kosong!!!") || console.log("Silakan isi umur anda terlebih dahulu!") : hasilumur()
+}
+const hasilumur = () => {
+    old=umurSaya
+    setTimeout( () => console.log (old+" tahun") ,2000);
+    setTimeout( () => console.log ("Umur Anda adalah") ,1000) ;
+}
